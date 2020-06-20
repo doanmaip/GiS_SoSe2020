2315ac7d78588484de44a133a85f53f2e00ed488
Mai <mai.phuong.doan@hs-furtwangen.de>
2020-06-20 23:25:20 +0200

    ts

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -1,23 +1,19 @@
 "use strict";
 var Aufgabe08;
-(function(Aufgabe08) {
-    let formData;
-    let myButton = document.getElementById("button");
-    myButton.addEventListener("click", buttonHandler);
-    async function addToURL() {
-        formData = new FormData(document.forms[0]);
-        let url = "https://mifungaa.herokuapp.com/";
+(function (Aufgabe08) {
+    let button = document.getElementById("button");
+    button.addEventListener("click", communiacate);
+    async function communiacate() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://mifungaa.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        return url;
-    }
-    async function buttonHandler() {
-        getResponse(await addToURL());
-    }
-    async function getResponse(_url) {
-        let response = await fetch(_url, { method: "get" });
-        let resp2 = await response.text();
-        console.log(resp2);
+        await fetch(url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=script.js.map
