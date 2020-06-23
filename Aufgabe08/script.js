dc281f1c788500a49630d23b8cf295b1329c8cd0
Mai <mai.phuong.doan@hs-furtwangen.de>
2020-06-23 14:27:21 +0200

    bug fix

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -1,15 +1,19 @@
 "use strict";
 var Aufgabe08;
 (function (Aufgabe08) {
+    let formData;
     let button = document.getElementById("button");
     button.addEventListener("click", communiacate);
     async function communiacate() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://mifungaa.herokuapp.com";
+        formData = new FormData(document.forms[0]);
+        let url = "https://mifungaa.herokuapp.com/";
+        //let url: string = "http://localhost:8100";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        await fetch(url);
+        let response = await fetch(url, { method: "get" });
+        let responseString = await response.text();
+        console.log(responseString);
         for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,15 +1,19 @@
 namespace Aufgabe08 {
-
+    let formData: FormData;
     let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
     button.addEventListener("click", communiacate);
 
     async function communiacate(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://mifungaa.herokuapp.com";
+        formData = new FormData(document.forms[0]);
+        let url: string = "https://mifungaa.herokuapp.com/";
+        //let url: string = "http://localhost:8100";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
-        await fetch(url);
+       
+        let response: Response = await fetch(url, { method: "get" });
+        let responseString: string = await response.text(); 
+        console.log(responseString);
 
         for (let entry of query) {
             console.log(entry);
